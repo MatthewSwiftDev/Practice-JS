fd864c635a7efab7bedeb1b298c8a66e857e626e
Strizhenok Matvey <strizhenok95@gmail.com>
2020-05-21 21:46:41 +0300

    List & range

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// Оработчик checkbox
 function funcConditionCheckBox() {
     let checkBoxElement = document.getElementById('one')
 
@@ -10,8 +10,9 @@ function funcConditionCheckBox() {
     }
 }
 
+// Обработчик radiobuttons
 let showResultButton = document.getElementById('B1')
- showResultButton.addEventListener('click', funcConditionRadioButton)
+showResultButton.addEventListener('click', funcConditionRadioButton)
 
 function funcConditionRadioButton(){
     let arryRadioButton = document.getElementsByName('radioButton1')
@@ -20,4 +21,30 @@ function funcConditionRadioButton(){
             alert('Выбран элемент: '+arryRadioButton[i].value)
         }
     }
+}
+
+// Обаработчик выпадающего списка
+let listElement = document.getElementById('list1')
+listElement.addEventListener('change', funcConditionList)
+
+function funcConditionList() {
+    let index = listElement.selectedIndex
+    console.log(index)
+    let listOptions = document.getElementById('list1').options[index].text
+    console.log(listOptions)
+    alert("Выбран: " + listOptions)  
+}
+
+// Обработчик range
+let rangeElement = document.getElementById('range1')
+let pElement = document.getElementById('p1')
+let inputTextElement = document.getElementById('text1')
+let divValueElement = document.getElementById('divValue')
+
+rangeElement.addEventListener('input', funcConditionRange)
+
+function funcConditionRange() {
+    pElement.innerText = rangeElement.value
+    inputTextElement.value = rangeElement.value
+    divValueElement.style.width = rangeElement.value + 'px'
 }
